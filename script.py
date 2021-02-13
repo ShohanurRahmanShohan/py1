9af8cc284ebf372a6e9901de51170be035ec8269
ShohanurRahmanShohan <78851871+ShohanurRahmanShohan@users.noreply.github.com>
2021-02-13 15:34:03 +0600

    Create script.py

---
+++ script.py
@@ -0,0 +1,18 @@
+#--*Sigret text*--
+
+# Owner : Shohanur Rahman 
+
+# version : 1.0
+
+User = input("Your username : ")
+
+if user=="Shohan":
+
+    print ("welcome " + "User ")
+
+    print (" This is your sigret text ")
+
+else : 
+
+  print ("check your password or username ")
+
